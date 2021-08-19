d1cd970bf2119919cf16a9be15ab7a09fdcd2e1c
Vũ Thành Nam <71971442+namsdai1@users.noreply.github.com>
2021-08-19 21:35:46 +0700

    Kiet ngy

--- src/screens/RegisterScreen.js
+++ src/screens/RegisterScreen.js
@@ -4,7 +4,7 @@ import {StyleSheet, Text, View} from 'react-native';
 const RegisterScreen = () => {
   return (
     <View>
-      <Text>Register Update Code</Text>
+      <Text>Register Upds</Text>
     </View>
   );
 };
