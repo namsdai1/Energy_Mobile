805edb8ced5904a5684e47b0789c6b3e8d8048b6
NguyenHoaiBao <japanhuynh1211@gmail.com>
2021-08-19 22:36:57 +0700

    test aaaaaaa

--- src/screens/PlashScreen.js
+++ src/screens/PlashScreen.js
@@ -5,7 +5,7 @@ import styles from '../styles/PlashStyle';
 const PlashScreen = () => {
     return (
         <View style={styles.container}>
-            <Text>Tao deo lam dau dfhgfhgsffsdmrgdfay tmlldfffsl</Text>
+            <Text>aaaaaaaaaaaaaa</Text>
         </View>
     )
 }
