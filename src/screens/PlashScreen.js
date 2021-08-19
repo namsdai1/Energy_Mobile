947afed4a47044a9f94d9de5a463bb715985cb1d
NguyenHoaiBao <japanhuynh1211@gmail.com>
2021-08-19 21:35:06 +0700

    t up vo plashscreen roi

--- src/screens/PlashScreen.js
+++ src/screens/PlashScreen.js
@@ -5,7 +5,7 @@ import styles from '../styles/PlashStyle';
 const PlashScreen = () => {
     return (
         <View style={styles.container}>
-            <Text>Hello welcome to Plash</Text>
+            <Text>Tao deo lam dau may tml</Text>
         </View>
     )
 }
