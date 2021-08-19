ad8910b513dce8ca3b9879a6ff1fc9105b451be7
kietntps13478 <nntv1507@gmail.com>
2021-08-19 22:59:37 +0700

    update1

--- src/screens/RegisterScreen.js
+++ src/screens/RegisterScreen.js
@@ -4,11 +4,7 @@ import {StyleSheet, Text, View} from 'react-native';
 const RegisterScreen = () => {
   return (
     <View>
-<<<<<<< HEAD
       <Text>Co cl chu t code</Text>
-=======
-      <Text>Register Upds</Text>
->>>>>>> bbec896a08e02ecde2e10b666d9b5a3f041930c4
     </View>
   );
 };
