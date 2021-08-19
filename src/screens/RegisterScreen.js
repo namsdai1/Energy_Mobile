b6e04f11c7dc9137af1534fe3b6531dff5f01f72
kietntps13478 <nntv1507@gmail.com>
2021-08-19 20:39:24 +0700

    Update code register

--- src/screens/RegisterScreen.js
+++ src/screens/RegisterScreen.js
@@ -1,13 +1,12 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
 
 const RegisterScreen = () => {
-    return (
-        <View>
-            <Text>Register</Text>
-        </View>
-    )
-}
+  return (
+    <View>
+      <Text>Register Update Code</Text>
+    </View>
+  );
+};
 
-export default RegisterScreen
-
+export default RegisterScreen;
